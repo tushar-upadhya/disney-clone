102fbdb0d1aa999b9ba464ea7dbfb9018bd12782
Tushar Upadhyay <tusharupadhyay691@gmail.com>
2023-06-08 13:16:08 +0530

    genre

--- src/api/api.js
+++ src/api/api.js
@@ -3,18 +3,13 @@ import axios from "axios";
 const URL = "https://api.themoviedb.org/3";
 const KEY = "0edcb55da40f8f65f8ee494d59b40aea";
 
-const fetchTrendingVideos = async () => {
-    try {
-        const response = await axios.get(
-            `${URL}/trending/all/day?api_key=${KEY}`
-        );
-        return response.data;
-    } catch (error) {
-        console.error("Error fetching trending videos:", error);
-        return null;
-    }
-};
+const Genre_URL =
+    "https://api.themoviedb.org/3/discover/movie?api_key=2ec0d66f5bdf1dd12eefa0723f1479cf";
+
+const fetchTrendingVideos = axios.get(URL + "/trending/all/day?api_key=" + KEY);
+const GenreId = (id) => axios.get(Genre_URL + "&with_genres=" + id);
 
 export default {
     fetchTrendingVideos,
+    GenreId,
 };
